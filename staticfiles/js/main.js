c239ef72ed6d8d0e3a4ec5ef3d258c86936651c3
Kashmir Kalirai <kashmirk67@gmail.com>
2017-10-09 09:34:00 +0100

    ran collectstatic

---
+++ staticfiles/js/main.js
@@ -0,0 +1,20 @@
+ var myHeading = document.querySelector('title');
+ myHeading.textContent = 'M H Maintenance Services';
+
+ //document.getElementById("demo").innerHTML = "Hello JavaScript!";
+
+//$(document).ready(function() {
+//  $("h2").addClass("underline");
+//  $("nav").addClass("border");
+//});
+
+//$(".myClass");  // selects HTML elements with class "myClass"
+//$("#bigButton");  // selects HTML element with id "bigButton"
+//$("li").css("color","red"); //Change the text of all <li>’s to red
+
+function myFunction() {
+    alert('You must be logged in to access the blog', "M H Maintenace Servcies");
+}
+ //document.querySelector('html').onclick = function() {
+//    alert('Ouch! Stop poking me!');
+//};
